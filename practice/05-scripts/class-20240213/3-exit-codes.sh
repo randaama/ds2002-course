72420896d9b5bd30d7825e9ff67ae11007b098dd
randaama <gba4fj@virginia.edu>
2024-02-14 01:25:03 -0500

    update with class practice

--- practice/05-scripts/class-20240213/3-exit-codes.sh
+++ practice/05-scripts/class-20240213/3-exit-codes.sh
@@ -3,11 +3,11 @@
 # Evaluate the status from a command
 date
 
-if [ $? -eq 0 ]
+if [ $? -eq 0 ] # if statement, if the exit code equals 0, then
 then
   echo "Success"
   exit 0
 else
-  echo "Failure" >&2
+  echo "Failure" >&2 # don't return
   exit 1
-fi
+fi # to end the if statement, if backwards (common syntax)
